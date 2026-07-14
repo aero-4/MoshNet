9306d6afd41dbd714d83d68adaa8400dbdec6cbb
aero-4 <dimongames6@gmail.com>
2026-07-14 10:42:01 +0300

    fix: convert to json result

--- backend/src/domains/usecase/domain_analyze.py
+++ backend/src/domains/usecase/domain_analyze.py
@@ -1,3 +1,5 @@
+import json
+
 from redis.asyncio import Redis
 
 from domains.domain.entities import Domain
@@ -7,10 +9,10 @@ from domains.infrastructure.services.domain_analyze import DomainsAnalyze
 async def start_domain_analyze(domain: Domain, domain_dep: DomainsAnalyze, redis: Redis):
     value = await redis.get(domain.domain)
     if value:
-        return value
+        return json.loads(value)
 
     result = await domain_dep.run(domain)
     await redis.set(domain.domain,
-                    result.model_dump(mode="json"),
+                    json.dumps(result.model_dump(mode="json")),
                     ex=10000)
     return result
