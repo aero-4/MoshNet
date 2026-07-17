84ea2e74510c9a33252709ef91fbcf602f129d3a
aero-4 <dimongames6@gmail.com>
2026-07-17 11:35:09 +0300

    fix: color metric

--- frontend/src/components/common/Metric.tsx
+++ frontend/src/components/common/Metric.tsx
@@ -5,7 +5,7 @@ type MetricProps = {
 
 export function Metric({label, value, color="bg-slate-50"}: MetricProps) {
     return (
-        <div className={`${color} rounded-md bg-slate-50 px-3 py-3`} >
+        <div className={`${color} rounded-md px-3 py-3`} >
             <div className="text-xs font-medium uppercase text-slate-100">{label}</div>
             <div className="mt-1 text-2xl font-semibold text-slate-200">{value}</div>
         </div>
